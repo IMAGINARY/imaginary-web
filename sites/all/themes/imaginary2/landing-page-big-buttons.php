83f2a44df3e39fbc4ed1b5a08d968575df1a1c78
Eric Londaits <eric.londaits@gmail.com>
2014-12-11 10:55:06 -0300

    Replaces MATHS IN AFRICA big button for TEXTS big button in the Home page.

--- sites/all/themes/imaginary2/landing-page-big-buttons.php
+++ sites/all/themes/imaginary2/landing-page-big-buttons.php
@@ -56,19 +56,6 @@ $size = ' width="400" height="220" ';
     		</div>
     	</a>
 
-        <a href="background-materials">
-    		<div class="big-button texts random hidden">
-    			<div class="inner">
-    				<div class="text">
-    					<h2>DOCUMENTS</h2>
-    					<p>read math background info</p>
-    				</div>
-        			<div class="background-image"><img src="<?php echo $themePath; ?>texts.jpg" alt="image1"<?php echo $size; ?>>
-        			</div>
-    			</div>
-    		</div>
-    	</a>
-
         <a href="/exhibitions">
     		<div class="big-button exhibitions random hidden">
     			<div class="inner">
@@ -117,17 +104,16 @@ $size = ' width="400" height="220" ';
     		</div>
     	</a>
 
-    	<a href="/aims">
-    		<div class="big-button africa">
-    				<div class="inner">
-						<div class="text">
-    						<h2>MATHS IN AFRICA</h2>
-							<p>join our upcoming workshop</p>
-						</div>
-						<div class="background-image">
-							<img src="<?php echo $themePath; ?>africa.jpg" alt="image1"<?php echo $size; ?>>
-						</div>
-					</div>
+        <a href="/background-materials">
+    		<div class="big-button texts">
+    			<div class="inner">
+    				<div class="text">
+    					<h2>TEXTS</h2>
+    					<p>read math background info</p>
+    				</div>
+        			<div class="background-image"><img src="<?php echo $themePath; ?>texts.jpg" alt="image1"<?php echo $size; ?>>
+        			</div>
+    			</div>
     		</div>
     	</a>
     </div>
