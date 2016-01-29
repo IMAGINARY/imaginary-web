e57777b1db05ed9db6f9490fd5fb3b3c0e2aaad5
Eric Londaits <eric.londaits@gmail.com>
2016-01-29 10:21:36 -0300

    Changes social media icons from gifs to Font Awesome

--- sites/all/themes/imaginary3/footer-social-media.php
+++ sites/all/themes/imaginary3/footer-social-media.php
@@ -1,22 +1,10 @@
-<?php
-global $theme;
-$pathToTheme = drupal_get_path('theme', $theme);
-$themePath =  "/".$pathToTheme."/images/social-media/";
-$size = ' width="30" height="30" ';
-?>
-
-
-
-
-
-
 <div id="social-media">
 	<div class="social facebook">
-		<a href="https://www.facebook.com/imaginary.exhibition"><img src="<?php echo $themePath; ?>facebook.png" alt="facebook"<?php echo $size; ?>/></a>
+		<a href="https://www.facebook.com/imaginary.exhibition"><i class="fa fa-facebook-official"></i></a>
 	</div>
 
 	<div class="social twitter">
-		<a href="https://twitter.com/imaginaryex"><img src="<?php echo $themePath; ?>twitter.png" alt="twitter"<?php echo $size; ?>/></a>
+		<a href="https://twitter.com/imaginaryex"><i class="fa fa-twitter"></i></a>
 	</div>
 
 </div>
