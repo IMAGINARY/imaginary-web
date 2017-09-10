c9a7e29ac949df0466634c039f687128f9621d8e
Eric Londaits <eric.londaits@gmail.com>
2017-09-10 19:27:40 -0300

    Event map: Various corrections to JS after static analysis.

--- sites/all/modules/imaginary_event/js/imaginary_event.js
+++ sites/all/modules/imaginary_event/js/imaginary_event.js
@@ -1,76 +1,80 @@
 (function($){
   "use strict";
+  /* global Drupal */
+  /* global MarkerClusterer */
+  /* global google */
 $(function(){
 
-  var markerClickHandler = function(ev) {
+var markerClickHandler = function() {
 
-    window.location.href = this.url;
-  };
+  window.location.href = this.url;
+};
 
-  var added = {};
+var added = {};
 
-  function addMarker(map, event) {
+function addMarker(map, event) {
 
-    var lat = Number(event.lat);
-    var lon = Number(event.lon);
-    var key = event.lat + "," + event.lon;
-    if(added[key] !== undefined) {
-      lat = lat + (Math.random() - 0.5) / 1500;
-      lon = lon + (Math.random() - 0.5) / 1500;
-    } else {
-      added[key] = true;
-    }
-
-    var marker = new google.maps.Marker({
-      position: {lat: lat, lng: lon},
-      map: map,
-      title: event.title,
-      url: event.url
-    });
-
-    marker.addListener('click', markerClickHandler);
-
-    return marker;
+  var lat = Number(event.lat);
+  var lon = Number(event.lon);
+  var key = event.lat + "," + event.lon;
+  if(added[key] !== undefined) {
+    lat = lat + (Math.random() - 0.5) / 1500;
+    lon = lon + (Math.random() - 0.5) / 1500;
+  } else {
+    added[key] = true;
   }
 
-  function displayMap(container, markerData) {
+  var marker = new google.maps.Marker({
+    position: {lat: lat, lng: lon},
+    map: map,
+    title: event.title,
+    url: event.url
+  });
 
-    var map = new google.maps.Map(container, {
-      zoom: 2,
-      minZoom: 1,
-      center: {lat: 18, lng: 0}
-    });
+  marker.addListener('click', markerClickHandler);
 
-    window.myMap = map;
+  return marker;
+}
 
-    var markers = [];
-    for (var i = 0; i !== markerData.events.length ; i++) {
+function displayMap(container, events) {
 
-      markers.push(addMarker(map, markerData.events[i]));
-    }
+  var map = new google.maps.Map(container, {
+    zoom: 2,
+    minZoom: 1,
+    center: {lat: 18, lng: 0}
+  });
 
-    var markerCluster = new MarkerClusterer(map, markers, {
-      imagePath: Drupal.settings.imaginaryEvent.clustererImages,
-    });
+  window.myMap = map;
+
+  var markers = [];
+  for (var i = 0; i !== events.length ; i++) {
+
+    markers.push(addMarker(map, events[i]));
   }
 
-  $.ajax(Drupal.absoluteUrl(Drupal.settings.api.events), {
-      dataType: 'json',
-      success: function(data) {
+  var markerCluster = new MarkerClusterer(map, markers, {
+    gridSize: 40,
+    imagePath: Drupal.settings.imaginaryEvent.clustererImages
+  });
+}
 
-        $('.imaginary_event-map').each(function() {
-          displayMap(this, data);
-        });
+$.ajax(Drupal.absoluteUrl(Drupal.settings.api.events), {
+    dataType: 'json',
+    success: function(data) {
 
-        $('[data-component=year-grouped-event-list]').each(function(){
-          window.IMAGINARY.YearGroupedEventList(this, data.events);
-        });
+      $('.imaginary_event-map').each(function() {
+        displayMap(this, data.events);
+      });
 
-        $('[data-component=country-grouped-event-list]').each(function(){
-          window.IMAGINARY.CountryGroupedEventList(this, data.events);
-        });
-      },
+      $('[data-component=year-grouped-event-list]').each(function(){
+        window.IMAGINARY.YearGroupedEventList(this, data.events);
+      });
+
+      $('[data-component=country-grouped-event-list]').each(function(){
+        window.IMAGINARY.CountryGroupedEventList(this, data.events);
+      });
     }
-  );
+  }
+);
 });
 })(jQuery);
