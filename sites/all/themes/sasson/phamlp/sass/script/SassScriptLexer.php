82b530cc348ed56850971b89228d83941aa7aaaf
Eric Londaits <eric.londaits@gmail.com>
2020-11-13 17:43:29 -0300

    Fixes built-in SASS compiler (themes/sasson/phamlp/sass) which times out in PHP 7.1 and above.
    See https://www.drupal.org/project/sasson/issues/2997003

--- sites/all/themes/sasson/phamlp/sass/script/SassScriptLexer.php
+++ sites/all/themes/sasson/phamlp/sass/script/SassScriptLexer.php
@@ -20,7 +20,7 @@ require_once('SassScriptVariable.php');
 /**
  * SassScriptLexer class.
  * Lexes SassSCript into tokens for the parser.
- * 
+ *
  * Implements a {@link http://en.wikipedia.org/wiki/Shunting-yard_algorithm Shunting-yard algorithm} to provide {@link http://en.wikipedia.org/wiki/Reverse_Polish_notation Reverse Polish notation} output.
  * @package			PHamlP
  * @subpackage	Sass.script
@@ -40,7 +40,7 @@ class SassScriptLexer {
 	public function __construct($parser) {
 		$this->parser = $parser;
 	}
-	
+
 	/**
 	 * Lex an expression into SassScript tokens.
 	 * @param string expression to lex
@@ -55,13 +55,13 @@ class SassScriptLexer {
 			}
 			elseif (($match = SassScriptFunction::isa($string)) !== false) {
 				preg_match(SassScriptFunction::MATCH_FUNC, $match, $matches);
-				
+
 				$args = array();
 				foreach (SassScriptFunction::extractArgs($matches[SassScriptFunction::ARGS])
 						as $expression) {
 					$args[] = $this->parser->evaluate($expression, $context);
 				}
-				
+
 				$tokens[] = new SassScriptFunction(
 						$matches[SassScriptFunction::NAME], $args);
 			}
@@ -74,7 +74,7 @@ class SassScriptLexer {
 			elseif (($match = SassColour::isa($string)) !== false) {
 				$tokens[] = new SassColour($match);
 			}
-			elseif (($match = SassNumber::isa($string)) !== false) {				
+			elseif (($match = SassNumber::isa($string)) !== false) {
 				$tokens[] = new SassNumber($match);
 			}
 			elseif (($match = SassScriptOperation::isa($string)) !== false) {
@@ -93,13 +93,17 @@ class SassScriptLexer {
 						}
 					}
 					$match .= $_string[0];
-					$_string = substr($_string, 1);			
+					$_string = substr($_string, 1);
 				}
 				$tokens[] = new SassString($match);
-			}			
+			}
 			$string = substr($string, strlen($match));
+
+      if ($string === '') {
+        $string = false;
+      }
 		}
-		return $tokens; 
+		return $tokens;
 	}
 
 	/**
