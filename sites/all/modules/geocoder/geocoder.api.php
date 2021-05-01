e1ccfae0cf1d130c5b9f8393b82446878e6c970b
Eric Londaits <eric.londaits@gmail.com>
2021-05-01 18:18:54 -0300

    Rever geocoder module to v1.4

--- sites/all/modules/geocoder/geocoder.api.php
+++ sites/all/modules/geocoder/geocoder.api.php
@@ -1,17 +1,11 @@
 <?php
 
 /**
- * @file
- * geocoder.api.php
- */
-
-/**
  * Implements hook_entity_property_info_alter().
  *
- * Give Users a virtual property that contains all relevant address information
- * from multiple sources.
+ * Give Users a virtual property that contains all relevant address information from multiple sources.
  */
-function hook_entity_property_info_alter(&$info) {
+function YOURMOD_entity_property_info_alter(&$info) {
   $info['user']['bundles']['user']['properties']['full_address'] = array(
     'type' => 'text',
     'label' => 'Full adress (from other fields)',
@@ -22,10 +16,9 @@ function hook_entity_property_info_alter(&$info) {
 /**
  * Entity property callback for user.full_address.
  *
- * Field_address is a text field, and when combined with a variable makes the
- * full address.
+ * field_address is a text field, and when combined with a variable makes the full address.
  */
-function hook_user_full_address($entity, $options, $field, $entity_type, $property) {
+function YOURMOD_user_full_address($entity, $options, $field, $entity_type, $property) {
   return $entity->field_address[LANGUAGE_NONE][0]['value'] . ', ' . variable_get('YOURMOD_global_country', 'Zimbabwe');
 }
 
@@ -34,8 +27,8 @@ function hook_user_full_address($entity, $options, $field, $entity_type, $proper
  *
  * Alter the source values before geocode them.
  */
-function hook_geocoder_geocode_values_alter(&$source_field_values, $field_info, $handler_settings, $field_instance) {
-  foreach ($source_field_values as $key => $value) {
+function YOURMOD_geocoder_geocode_values_alter(&$source_field_values, $field_info, $handler_settings, $field_instance) {
+  foreach($source_field_values as $key => $value) {
     $source_field_values[$key]['administrative_area'] = 'Ciudad de Buenos Aires';
   }
 }
